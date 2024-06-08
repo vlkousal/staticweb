2b47d0ac7a2406d6e1776bd891df3fa9877c52f3
Vladimír Kousal <vladimirkousal@protonmail.com>
2024-06-08 14:39:20 +0200

    win95 feel

---
+++ current_time.js
@@ -0,0 +1,21 @@
+
+function updateTime() {
+	const timeContainer = document.getElementById('time-container');
+	const now = new Date();
+
+	let hours = now.getHours();
+	const minutes = now.getMinutes();
+	const ampm = hours >= 12 ? 'PM' : 'AM';
+
+	hours = hours % 12;
+	hours = hours ? hours : 12;
+
+	const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+	timeContainer.textContent = `${hours}:${formattedMinutes} ${ampm}`;
+	console.log(hours + ':' + formattedMinutes + " " + ampm);
+}
+
+updateTime();
+console.log(":)");
+setInterval(updateTime, 1000);
---
+++ cursors/readme.txt
@@ -0,0 +1,21 @@
+﻿=== Original Windows 95/98 Cursor Set ===
+
+By: darix555
+
+Converted by: coolkase
+
+Download: http://www.rw-designer.com/cursor-set/win-95-98
+
+	  https://drive.google.com/drive/folders/1-Dhqpw-gcu-qoOsxF1aePH9AGOMAZC-8?usp=sharing
+
+Author's description:
+
+Original Win 95/98 cursors hope you like it! I replaced the working in backgroud (wib) hourglass with a cd because it looks more better and cute :-)
+
+==========
+
+License: Released to Public Domain
+
+You are free:
+
+* To use this work for any legal purpose.
