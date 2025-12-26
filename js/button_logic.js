5eb21e09f39fdd5c9029e60c7246261e77c15070
Vladimír Kousal <vladimirkousal@protonmail.com>
2025-12-26 19:26:18 +0100

    added the chinese transcription trainer

--- js/button_logic.js
+++ js/button_logic.js
@@ -9,11 +9,28 @@ function toggleStartMenu() {
 
 function openWelcome() {
 	document.getElementById("welcome-frame").hidden = false;
+
+	document.getElementById("map-frame").hidden = true;
+	document.getElementById("trainer-frame").hidden = true;
+
 	document.getElementById("start-menu").hidden = true;
 }
 
 function openMap() {
 	document.getElementById("map-frame").hidden = false;
+
+	document.getElementById("welcome-frame").hidden = true;
+	document.getElementById("trainer-frame").hidden = true;
+
+	document.getElementById("start-menu").hidden = true;
+}
+
+function openTrainer() {
+	document.getElementById("trainer-frame").hidden = false;
+
+	document.getElementById("welcome-frame").hidden = true;
+	document.getElementById("map-frame").hidden = true;
+
 	document.getElementById("start-menu").hidden = true;
 }
 
@@ -23,5 +40,8 @@ function closeWelcome() {
 
 function closeMap() {
 	document.getElementById("map-frame").hidden = true;
+}
 
+function closeTrainer() {
+	document.getElementById("trainer-frame").hidden = true;
 }
